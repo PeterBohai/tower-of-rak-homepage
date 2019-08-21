1c40957e32bac656641da4b199db7f7b8eb97f42
Peter Hu <peterbohai@gmail.com>
2019-08-20 20:16:08 -0700

    Add specific node version for heroku and add rules to gitignore

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.post("/", function(req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function (){
-	console.log("Server started on port 3000...");
+	console.log("Server started successfully");
 });
