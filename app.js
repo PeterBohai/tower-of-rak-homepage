d4a33f0c62cb3461f72cf842afa936b404e4d343
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2023-02-07 22:09:28 -0600

    Reformat other files with prettier

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res) {
-	res.download(__dirname + "/downloads/tower-of-rak.zip");
+app.post("/", function (req, res) {
+    res.download(__dirname + "/downloads/tower-of-rak.zip");
 });
 
-app.listen(process.env.PORT || 3000, function (){
-	console.log("Server started successfully");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server started successfully");
 });
--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-$(document).on("click", "[data-toggle='lightbox']", function(event) {
-	event.preventDefault();
-	$(this).ekkoLightbox();
+$(document).on("click", "[data-toggle='lightbox']", function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
 });
